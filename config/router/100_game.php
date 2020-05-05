74b8a2733f0dd88c8333c262b99d2dbec083f720
Kristoffer Markow <Kristoffer.markow@gmail.com>
2020-05-05 19:52:44 +0100

    Mounting my game to route

---
+++ config/router/100_game.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Dice controller.
+ */
+return [
+    "routes" => [
+        [
+            "info" => "Dice controller",
+            "mount" => "dice1",
+            "handler" => "\Kris3XIQ\Dice1\DiceController",
+        ],
+    ]
+];
