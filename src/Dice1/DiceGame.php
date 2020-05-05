9cf0c49dc8e8e1d63f33c8dc707da0fe14cd1a5c
Kristoffer Markow <Kristoffer.markow@gmail.com>
2020-05-05 19:55:57 +0100

    Modified version of the dicegame moved to dice1 catalogue

---
+++ src/Dice1/DiceGame.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Kris3XIQ\Dice1;
+
+/**
+ * Guess my number, a class supporting the game through GET, POST and SESSION.
+ */
+class DiceGame
+{
+    /**
+     * Constructor to initiate the object with current game settings,
+     * if available. Randomize the current number if no value is sent in.
+     *
+     * @param int $players The amount of players a dice game should have,
+     *                     defaults to 2 players.
+     * 
+     */
+    //int $players = 2
+    public function __construct(int $players = 2) 
+    {
+        $this->players = [];
+
+        for ($i = 0; $i < $players; $i++) {
+            $this->players[] = new DicePlayer();
+        }
+    }
+
+    public function getPlayer($player) 
+    {
+        $playerName = array_slice($this->players, $player, 1, true);
+        return $playerName;
+    }
+}
